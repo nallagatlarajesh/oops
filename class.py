9ec27f04f640afb6bc232f3b5d084496ba0a88f3
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2023-10-12 15:32:54 +0530

    Create class.py
    example for class program

---
+++ class.py
@@ -0,0 +1,16 @@
+
+class dog:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+        
+    def bark(self):
+        print(f"{self.name} barks!")
+
+#creating objects (instances ) of the dog class
+dog1=dog("buddy ",3)
+dog2=dog("rex",5)
+
+#accessing attributes and calling methodes
+print(dog1.name)    
+dog2.bark()
